fabdb095812ee79a5d50280526c012804d92b04a
PARTHSHARMA4010 <2023kucp1020@iiitkota.ac.in>
2024-07-19 18:21:43 +0530

    carControllers is completed and working fine

--- backend/controllers/carControllers.js
+++ backend/controllers/carControllers.js
@@ -3,7 +3,7 @@ import { Car } from "../models/car.model.js";
 import ErrorHandler from "../middlewares/error.js";
 
 export const getAllcars = catchasyncError(async (req, res, next) => {
-  const cars = await Car.find({ expired: false });
+  const cars = await Car.find({ sold : false });
   res.status(200).json({
     success: true,
     cars,
@@ -67,3 +67,78 @@ export const sellCar = catchasyncError(async (req, res, next) => {
 });
 
 
+export const getMyCars = catchasyncError(async (req, res, next) => {
+  const { option } = req.user;
+  if (option === "Buyer") {
+    return next(
+      new ErrorHandler("You are Buyer", 400)
+    );
+  }
+  const myCars = await Car.find({ postedBy: req.user._id });
+  res.status(200).json({
+    success: true,
+    myCars,
+  });
+});
+
+export const updateCar = catchasyncError(async (req, res, next) => {
+  const { option } = req.user;
+  if (option === "Buyer") {
+    return next(
+      new ErrorHandler("You are Buyer", 400)
+    );
+  }
+  const { id } = req.params;
+  let car = await Car.findById(id);
+  if (!car) {
+    return next(new ErrorHandler("OOPS! Car not found.", 404));
+  }
+  car = await Car.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Car status Updated!",
+  });
+});
+
+
+
+export const deleteCar = catchasyncError(async (req, res, next) => {
+  const { option } = req.user;
+  if (option === "Buyer") {
+    return next(
+      new ErrorHandler("You are Buyer", 400)
+    );
+  }
+  const { id } = req.params;
+  const car = await Car.findById(id);
+  if (!car) {
+    return next(new ErrorHandler("OOPS! Car not found.", 404));
+  }
+  await car.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Car Deleted!",
+  });
+});
+
+
+
+// export const getSingleCar = catchasyncError(async (req, res, next) => {
+//   const { id } = req.params;
+//   try {
+//     const car = await Car.findById(id);
+//     if (!car) {
+//       return next(new ErrorHandler("Car not found.", 404));
+//     }
+//     res.status(200).json({
+//       success: true,
+//       car,
+//     });
+//   } catch (error) {
+//     return next(new ErrorHandler(`Invalid ID / CastError`, 404));
+//   }
+// });
--- backend/routes/sellerRouter.js
+++ backend/routes/sellerRouter.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { getAllcars, sellCar } from '../controllers/carControllers.js'
+import { deleteCar, getAllcars, getMyCars, sellCar, updateCar } from '../controllers/carControllers.js'
 import { isAuth } from '../middlewares/auth.js'
 const router = express.Router()
 
 router.get("/getallcars", getAllcars)
 router.post("/sell",isAuth,sellCar)
+router.get("/getmycars",isAuth,getMyCars)
+router.put("/updatecar/:id",isAuth,updateCar)
+router.delete("/deletecar/:id",isAuth,deleteCar)
 
 export default router
